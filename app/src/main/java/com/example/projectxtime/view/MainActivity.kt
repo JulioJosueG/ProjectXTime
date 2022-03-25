c769ffee392c90e32885e1fb785ae8620b836da6
JulioJosueG <jh2019-0417@uce.edu.do>
2022-03-25 13:39:45 -0400

    packeges mvvm

--- app/src/main/java/com/example/projectxtime/view/MainActivity.kt
+++ app/src/main/java/com/example/projectxtime/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.projectxtime
+package com.example.projectxtime.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectxtime.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
