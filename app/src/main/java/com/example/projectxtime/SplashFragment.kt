c62d7b1e501a0ac6bb112b3385a32b027912d3e4
JulioJosueG <jh2019-0417@uce.edu.do>
2022-03-25 13:58:00 -0400

    Navigation and Structure

---
+++ app/src/main/java/com/example/projectxtime/SplashFragment.kt
@@ -0,0 +1,24 @@
+package com.example.projectxtime
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/projectxtime/StudentMenuFragment.kt
@@ -0,0 +1,24 @@
+package com.example.projectxtime
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class StudentMenuFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_student_menu, container, false)
+    }
+
+}
