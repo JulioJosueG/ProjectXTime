370b9532b79540de3ddb550711b4bcd8f1b0a572
Renso Soriano <rensomiguel1@gmail.com>
2022-03-25 16:45:26 -0400

    Añadido del splash screen para el proyecto

--- app/src/main/java/com/example/projectxtime/view/SplashFragment.kt
+++ app/src/main/java/com/example/projectxtime/view/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.example.projectxtime.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.projectxtime.R
 
 
@@ -19,7 +22,12 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+       val view = inflater.inflate(R.layout.fragment_splash, container, false)
+     Handler(Looper.myLooper()!!).postDelayed({
+         findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+     },5000)
+
+        return view
     }
 
 }
