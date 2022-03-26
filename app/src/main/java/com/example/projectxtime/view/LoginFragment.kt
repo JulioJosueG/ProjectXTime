b2fea6f8a5bff3c43a87ca877c81f5d47a158185
JulioJosueG <jh2019-0417@uce.edu.do>
2022-03-26 13:27:17 -0400

    Change Logo, Login.

--- app/src/main/java/com/example/projectxtime/view/LoginFragment.kt
+++ app/src/main/java/com/example/projectxtime/view/LoginFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import com.example.projectxtime.R
+import com.example.projectxtime.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +23,30 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+
+
+        binding.btnLogin.setOnClickListener{
+            if (binding.usertxt.text.contentEquals("admin") &&binding.usertxt.text.contentEquals("admin")){
+                it.findNavController().navigate(R.id.action_loginFragment_to_adminMenuFragment)
+
+            }
+            if (binding.usertxt.text.contentEquals("student") &&binding.usertxt.text.contentEquals("student")){
+                it.findNavController().navigate(R.id.action_loginFragment_to_studentMenuFragment)
+
+            }
+
+            if (binding.usertxt.text.contentEquals("teacher") &&binding.usertxt.text.contentEquals("teacher")){
+                it.findNavController().navigate(R.id.action_loginFragment_to_teacherMenuFragment)
+
+            }
+
+
+        }
+
+        return binding.root
     }
+
 
 
 }
