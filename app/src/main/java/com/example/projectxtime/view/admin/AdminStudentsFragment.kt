80a85274f43ddbe71ec4a66743a1c0c4d220118e
ABIGAIL RODRÍGUEZ MOTA <ar2019-0095@uce.edu.do>
2022-04-23 22:16:37 -0400

    Diseño de administrar estudiante.

--- app/src/main/java/com/example/projectxtime/view/admin/AdminStudentsFragment.kt
+++ app/src/main/java/com/example/projectxtime/view/admin/AdminStudentsFragment.kt
@@ -5,29 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.projectxtime.R
+import com.example.projectxtime.databinding.FragmentAdminStudentsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminStudentsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdminStudentsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding:FragmentAdminStudentsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +22,14 @@ class AdminStudentsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        binding = FragmentAdminStudentsBinding.inflate(inflater, container,false)
+
+        binding.btnFloatAdd.setOnClickListener {
+            it.findNavController().navigate(R.id.action_adminStudentsFragment_to_addStudentFragment)
+        }
+
         return inflater.inflate(R.layout.fragment_admin_students, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminStudentsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminStudentsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
