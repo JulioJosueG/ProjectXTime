99319d21ed2c6d4bd63ff85d89a6ac181ca4e436
SPFrancisco <86896732+SPFrancisco@users.noreply.github.com>
2022-04-24 03:57:50 -0400

    Teacher backup

--- app/src/main/java/com/example/projectxtime/teachers/AddEditTeacherFragment.kt
+++ app/src/main/java/com/example/projectxtime/teachers/AddEditTeacherFragment.kt
@@ -1,21 +1,73 @@
 package com.example.projectxtime.teachers
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.projectxtime.R
+import com.example.projectxtime.databinding.FragmentAddEditTeacherBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AddEditTeacherFragment : Fragment() {
 
-    private lateinit var binding : AddEditTeacherFragment
+    private lateinit var binding : FragmentAddEditTeacherBinding
 
-    private lateinit var database : FirebaseAuth
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // init database
+        database = FirebaseDatabase.getInstance().getReference("Profesores")
+
+        ValidateData()
+    }
+
+    private fun ValidateData() {
+        // validate data
+
+        if(TextUtils.isEmpty(binding.teacherNameEdit.text.toString())){
+            binding.teacherNameEdit.error = "This field is required"
+        }
+        else if(TextUtils.isEmpty(binding.teacherLastNameEdit.text.toString())){
+            binding.teacherLastNameEdit.error = "This field is required"
+        }
+        else{
+            // data is valid, continue signup
+            registateTeacher()
+        }
+    }
+
+    private fun registateTeacher() {
+
+        var gender : String ?= null;
+        if (binding.rbMen.isChecked){
+            gender = "Hombre"
+        }
+        else {
+            gender = "Mujer"
+        }
+
+        val teacherID = database.push().key.toString()
+        val teacher = Teacher(teacherID,
+        binding.teacherLastNameEdit.text.toString(),
+        binding.teacherLastNameEdit.text.toString(),
+        gender)
+
+        database.child(teacherID).setValue(teacher).addOnSuccessListener{
+
+        }
+        .addOnFailureListener { e->
+            // database user add failed
+            Toast.makeText(requireContext(), "Database register failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateView(
--- app/src/main/java/com/example/projectxtime/teachers/Teacher.kt
+++ app/src/main/java/com/example/projectxtime/teachers/Teacher.kt
@@ -1,7 +1,7 @@
 package com.example.projectxtime.teachers
 
 data class Teacher (
-    val id: Int = 0,
+    val id: String?=null,
     val name: String?=null,
     val lastName: String?=null,
     val gender: String?=null
