b87423507efe94fde86b33feb6e60de52ad16d17
JulioJosueG <jh2019-0417@uce.edu.do>
2022-04-25 18:40:08 -0400

    Teacher

--- app/src/main/java/com/example/projectxtime/view/TeacherMenuFragment.kt
+++ app/src/main/java/com/example/projectxtime/view/TeacherMenuFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.projectxtime.R
+import com.example.projectxtime.databinding.FragmentLoginBinding
+import com.example.projectxtime.databinding.FragmentTeacherMenuBinding
 
 
 class TeacherMenuFragment : Fragment() {
+    private lateinit var binding: FragmentTeacherMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,7 +23,15 @@ class TeacherMenuFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_teacher_menu, container, false)
+        binding = FragmentTeacherMenuBinding.inflate(inflater,container,false)
+
+        binding.imgSchedule.setOnClickListener{
+            it.findNavController().navigate(R.id.action_teacherMenuFragment_to_studentScheduleFragment)
+
+        }
+
+        return binding.root
+
     }
 
 
